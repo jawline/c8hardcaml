c635112586cae6a8d74a728495a7171e7fdad134
Blake Loring <blake@parsed.uk>
2021-11-03 00:10:08 +0800

    Reworking the executor

--- src/c8.ml
+++ src/c8.ml
@@ -195,11 +195,20 @@ module Registers = struct
     }
 end
 
+(* This module executes a single instruction *)
 module Executor = struct
+
+  module States = struct
+    type t =
+      | Wait
+      | Executing
+    [@@deriving sexp_of, compare, enumerate]
+  end
+
   module I = struct
     type 'a t =
       { clock : 'a
-      ; enable: 'a [@bits 1]
+      ; begin_: 'a [@bits 1]
       ; opcode: 'a [@bits 16]
       }
     [@@deriving sexp_of, hardcaml]
@@ -207,51 +216,131 @@ module Executor = struct
 
   module O = struct
     type 'a t = {
-      pc: 'a;
-      done_: 'a [@bits 1]
+      pc: 'a
+      ; error: 'a
+      ; done_: 'a [@bits 1]
     } [@@deriving sexp_of, hardcaml]
   end
 
   let create (i : _ I.t) =
     let open Always in
     let open Variable in
+    let state = Always.State_machine.create (module States) ~enable:vdd r_sync in
     let pc = reg ~enable:vdd ~width:12 r_sync in
     let _i = reg ~enable:vdd ~width:12 r_sync in
-    let _v = Sequence.map (Sequence.range 0 16) ~f:(fun _ -> reg ~enable:vdd ~width:8 r_sync) in
-    compile [
-    when_ (i.enable) [
-      pc <-- pc.value +:. 2;
+    let error = reg ~enable:vdd ~width:8 r_sync in
+    let executing_opcode = reg ~enable:vdd ~width:16 r_sync in
+    let done_ = reg ~enable:vdd ~width:1 r_sync in
+    let primary_op = select executing_opcode.value 15 12 in
+    let jump_location = select executing_opcode.value 11 0 in
+    let target_immediate = select executing_opcode.value 7 0 in
+    let registers = Sequence.map (Sequence.range 0 16) ~f:(fun _ -> reg ~enable:vdd ~width:8 r_sync) |> Sequence.to_list in
+
+    (* A with-valid list of the target register for one-hot encoding *)
+    let target_register = List.mapi registers ~f:(fun idx register ->
+        let target_register = (select executing_opcode.value 11 8) ==:. idx in
+        {
+          With_valid.valid = target_register
+        ; value = register.value
+        }) |> onehot_select
+    in
+    let ok = proc [
+      error <--. 0;
+      done_ <--. 1;
+      state.set_next Wait
     ]
+    in
+    compile [
+      state.switch [
+        Wait, [
+          when_ (i.begin_ ==:. 1) [
+            state.set_next Executing;
+            executing_opcode <-- i.opcode
+          ]
+        ];
+        Executing, [
+            (* This error state will become 0 if any op is matched *)
+            error <--. 1;
+            (* We use 0 (originally native code call) as a No-op *)
+            when_ (primary_op ==:. 0) [
+              pc <-- pc.value +:. 2;
+              ok
+            ];
+            (* Jump to the 12 bits at the end of the opcode *)
+            when_ (primary_op ==:. 1) [
+              pc <-- jump_location;
+              ok
+            ];
+            (* Skip the next instruction of register is equal to immediate *)
+            when_ (primary_op ==:. 3) [
+              if_ (target_register ==: target_immediate) [
+                (* Skip the next instruction *)
+                pc <-- pc.value +:. 4
+              ] [
+                (* We do not skip the next instruction *)
+                pc <-- pc.value +:. 2
+              ];
+              ok
+            ]
+        ]
+      ]
     ];
-    { O.pc = pc.value; done_ = vdd }
+    { O.pc = pc.value; done_ = done_.value; error = error.value }
   ;;
 
-  let test ~create ~enable ~stop_when =
+  let standard_stop error done_ = error <> 0 || done_ <> 0
+
+  let test ~create ~opcode ~stop_when =
     let module Simulator = Cyclesim.With_interface (I) (O) in
     let sim = Simulator.create create in
     let inputs : _ I.t = Cyclesim.inputs sim in
     let outputs : _ O.t = Cyclesim.outputs sim in
-    inputs.enable := Bits.of_int ~width:1 enable;
+    inputs.begin_ := (Bits.of_int ~width:1 1);
+    inputs.opcode := opcode;
     let step () =
       Cyclesim.cycle sim;
-      stop_when (Bits.to_int !(outputs.pc)) (Bits.to_int !(outputs.done_))
+      stop_when (Bits.to_int !(outputs.error)) (Bits.to_int !(outputs.done_))
     in
     let rec until ~f = if f () then () else until ~f in
     until ~f:step;
-    !(outputs.pc)
+    !(outputs.pc), !(outputs.error)
   ;;
 
   let%expect_test "step (disabled)" =
-    let pc = test ~enable:0 ~create ~stop_when:(fun _ _ -> true) in
+    let pc, _ = test ~opcode:(Bits.of_int ~width:16 0) ~create ~stop_when:standard_stop in
     let pc = Bits.to_int pc in
     Core.print_s [%message (pc : int)];
     [%expect {| (pc 0) |}]
   ;;
 
   let%expect_test "step (enabled)" =
-    let pc = test ~enable:1 ~create ~stop_when:(fun _ _ -> true) in
+    let pc, _ = test ~opcode:(Bits.of_int ~width:16 0) ~create ~stop_when:standard_stop in
     let pc = Bits.to_int pc in
     Core.print_s [%message (pc : int)];
+    [%expect {| (pc 2) |}]
+  ;;
+
+  let%expect_test "step (jump) to 1024" =
+    let jump_to_addr = Bits.of_string "16'b0001010000000000" in
+    let pc, error = test ~opcode:jump_to_addr ~create ~stop_when:standard_stop in
+    let pc, error = Bits.to_int pc, Bits.to_int error in
+    Core.print_s [%message (pc : int) (error : int)];
+    [%expect {| (pc 2) |}]
+  ;;
+
+  let%expect_test "step (jump) to 512" =
+    let jump_to_addr = Bits.of_string "16'b0001001000000000" in
+    let pc, error = test ~opcode:jump_to_addr ~create ~stop_when:standard_stop in
+    let pc, error = Bits.to_int pc, Bits.to_int error in
+    Core.print_s [%message (pc : int) (error : int)];
+    [%expect {| (pc 2) |}]
+  ;;
+
+  let%expect_test "step (jump) to 1" =
+    let jump_to_addr = Bits.of_string "16'b0001000000000001" in
+    let pc, error = test ~opcode:jump_to_addr ~create ~stop_when:standard_stop in
+    let pc, error = Bits.to_int pc, Bits.to_int error in
+    Core.print_s [%message (pc : int) (error : int)];
     [%expect {| (pc 2) |}]
   ;;
 end
